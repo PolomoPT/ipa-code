edef8bc106ec6b28fcc61141bd568101bd1def87
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-10-23 09:41:26 -0300

    Co-authored-by: metrokike <metrokike@users.noreply.github.com>

---
+++ consoante.py
@@ -0,0 +1,8 @@
+import re
+
+def consoante(foo):
+    return re.sub("qu","k",foo)
+    return re.sub ("j", "ʒ", foo)
+
+## for i in range(0 ,len(seleção), 2):
+        
---
+++ process.py
@@ -0,0 +1,6 @@
+import consoante
+import vogais
+
+seleção = input()
+pós_consoante = consoante.consoante(seleção)
+print(pós_consoante)
