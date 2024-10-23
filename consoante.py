64eaf77c7aeffd271b08bab0bd1f26ed58dd84b3
metrokike <metrokike2008yt@gmail.com>
2024-10-23 20:17:44 -0300

    salsicha

--- consoante.py
+++ consoante.py
@@ -4,7 +4,7 @@ def consoante(foo):
     foo = re.sub("qu","k",foo)
     foo = re.sub("j", "ʒ", foo)
     foo = re.sub("lh", "ʎ", foo) 
-    foo - re.sub("nh", "ɲ", foo)
+    foo = re.sub("nh", "ɲ", foo)
     return foo
 
 ## for i in range(0 ,len(seleção), 2):
