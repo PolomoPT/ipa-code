52f631b2865b0daa207222f44f992561b498b74c
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-10-23 21:45:53 -0300

    simplificar codigo vogais

--- vogais.py
+++ vogais.py
@@ -12,24 +12,14 @@ def finais(foo):
     return foo
 
 def nasais(foo):
-    if(re.search("a(?=" + classes.c_nasais + classes.c_vogais + ")", foo)):
-        foo = re.sub("a(?=" + classes.c_nasais + classes.c_vogais + ")", "ɐ̃", foo)
-    if(re.search("a(?=" + classes.c_nasais + ")(?=" + classes.c_consoantes + ")", foo)):
-        foo = re.sub("a" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ɐ̃", foo)
-    if(re.search("e(?=" + classes.c_nasais + classes.c_vogais + ")", foo)):
-        foo = re.sub("e(?=" + classes.c_nasais + classes.c_vogais + ")", "ẽ", foo)
-    if(re.search("e(?=" + classes.c_nasais + ")(?=" + classes.c_consoantes + ")", foo)):
-        foo = re.sub("e" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ẽ", foo)
-    if(re.search("i(?=" + classes.c_nasais + classes.c_vogais + ")", foo)):
-        foo = re.sub("i(?=" + classes.c_nasais + classes.c_vogais + ")", "ĩ", foo)
-    if(re.search("i(?=" + classes.c_nasais + ")(?=" + classes.c_consoantes + ")", foo)):
-        foo = re.sub("i" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ĩ", foo)
-    if(re.search("o(?=" + classes.c_nasais + classes.c_vogais + ")", foo)):
-        foo = re.sub("o(?=" + classes.c_nasais + classes.c_vogais + ")", "õ", foo)
-    if(re.search("o(?=" + classes.c_nasais + ")(?=" + classes.c_consoantes + ")", foo)):
-        foo = re.sub("o" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "õ", foo)
-    if(re.search("u(?=" + classes.c_nasais + classes.c_vogais + ")", foo)):
-        foo = re.sub("u(?=" + classes.c_nasais + classes.c_vogais + ")", "ũ", foo)
-    if(re.search("u(?=" + classes.c_nasais + ")(?=" + classes.c_consoantes + ")", foo)):
-        foo = re.sub("u" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ũ", foo)
+    foo = re.sub("a(?=" + classes.c_nasais + classes.c_vogais + ")", "ɐ̃", foo)
+    foo = re.sub("a" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ɐ̃", foo)
+    foo = re.sub("e(?=" + classes.c_nasais + classes.c_vogais + ")", "ẽ", foo)
+    foo = re.sub("e" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ẽ", foo)
+    foo = re.sub("i(?=" + classes.c_nasais + classes.c_vogais + ")", "ĩ", foo)
+    foo = re.sub("i" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ĩ", foo)
+    foo = re.sub("o(?=" + classes.c_nasais + classes.c_vogais + ")", "õ", foo)
+    foo = re.sub("o" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "õ", foo)
+    foo = re.sub("u(?=" + classes.c_nasais + classes.c_vogais + ")", "ũ", foo)
+    foo = re.sub("u" + classes.c_nasais + "(?=" + classes.c_consoantes + ")", "ũ", foo)
     return foo
