6c3ce95eabac2b069ac8d84048111ed7930c82b4
metrokike <metrokike2008yt@gmail.com>
2024-10-24 21:17:14 -0300

    C /s/

--- consoante.py
+++ consoante.py
@@ -10,4 +10,8 @@ def consoante(seleção):
     seleção = re.sub(r"r(?=[\s|$])", "ʁ", seleção)
     seleção = re.sub("rr", "ʁ", seleção)
     seleção = re.sub("r(?=["+classes.l_vogais+"])", "ɾ", seleção)
+    ## seleção re.sub ("b", "b", seleção)
+    seleção = re.sub("c(?=[e|é|ê|i|í])", "s", seleção)
+    ## seleção re.sub ("d", "d", seleção)
+    ## seleção re.sub ("f", "f", seleção)
     return seleção
