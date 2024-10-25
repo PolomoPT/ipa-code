2662081ddef7a67c8de2aaa50653ed7e2cf8d9de
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-10-25 10:17:44 -0300

    add fricated coda r

--- consoante.py
+++ consoante.py
@@ -7,7 +7,7 @@ def consoante(seleção):
     seleção = re.sub("lh", "ʎ", seleção) 
     seleção = re.sub("nh", "ɲ", seleção)
     seleção = re.sub(r"(?<=[\s|^])r", "ʁ", seleção)
-    seleção = re.sub(r"r(?=[\s|$])", "ʁ", seleção)
+    seleção = re.sub(r"r(?=[\s|$"+classes.consoantes+"])", "ʁ", seleção)
     seleção = re.sub("rr", "ʁ", seleção)
     seleção = re.sub("r(?=["+classes.l_vogais+"])", "ɾ", seleção)
     return seleção
