48353632243cd236da0d4c21e3a831c312c5a9d2
metrokike <metrokike2008yt@gmail.com>
2024-10-26 22:00:05 -0300

    consonants

--- consoante.py
+++ consoante.py
@@ -1,8 +1,10 @@
 import re
 import classes
-##CARALHO TROQUEI O ŋ PELO j̃ EM TUDO E A TRADUÇÃO DO NH PAROU DE FUNCIONAR. PQP PQ TUDO Q EU PROGRAMO DA ERRADO, SFD
+## *TROQUEI O ŋ PELO j̃ EM TUDO E A TRADUÇÃO DO NH PAROU DE FUNCIONAR
+## Consonants that doesn't change in IPA (e.x. b = /b/) are not included in the code.
 def consoante(seleção):
-    seleção = re.sub("qu","k",seleção)
+    seleção = re.sub("qu(?=[e|é|ê|i|í])","k",seleção)
+    seleção = re.sub("qu(?=[a|á|ã|â|o|ó|ô|õ|u|ú])","kʷ",seleção)
     seleção = re.sub("j", "ʒ", seleção)
     seleção = re.sub("lh", "ʎ", seleção) 
     seleção = re.sub("nh", "j̃", seleção)
@@ -10,12 +12,21 @@ def consoante(seleção):
     seleção = re.sub(r"r(?=[\s|$"+classes.consoantes+"])", "ʁ", seleção)
     seleção = re.sub("rr", "ʁ", seleção)
     seleção = re.sub("r(?=["+classes.l_vogais+"])", "ɾ", seleção)
-    ## seleção re.sub ("b", "b", seleção)
     seleção = re.sub("c(?=[e|é|ê|i|í])", "s", seleção)
+    seleção = re.sub("sh", "ʃ", seleção) 
     ## seleção = re.sub("c(?=[a|á|ã|â|o|ó|ô|õ|u|ú])", "k", seleção) não funciona, mas não há problemas.
-    ## E o seleção re.sub para o Ç?
-    ## seleção re.sub ("d", "d", seleção)
-    ## seleção re.sub ("f", "f", seleção)
+    ## Como será o código para o Ç?
+    seleção = re.sub("ch", "ʃ", seleção)
     ##seleção = re.sub("g(?=[e|é|ê|i|í])", "ʒ", seleção) não funciona, mas não há problemas.
     ##seleção = re.sub("g(?=[a|á|ã|â|o|ó|ô|õ|u|ú])", "g", seleção) não funciona, mas não há problemas.
+    ## seleção = re.sub ("s(?="+classes.l_vogais+")(?=<"+classes.l_vogais+")", "z", seleção) não funciona.
+    seleção = re.sub("s(?=\\s|^)", "s", seleção)
+    seleção = re.sub("s(?=<"+classes.consoantes+")", "s", seleção)
+    seleção = re.sub("sc", "s", seleção)
+    seleção = re.sub("sç", "s", seleção)
+    seleção = re.sub("ss", "s", seleção)
+    ## Como será o código para o X entre vogais, com som de  /ʃ/, /s/, /ks/ ou /z/?
+    seleção = re.sub("x(?=\\s|^)", "ʃ", seleção)
+    seleção = re.sub("x(?=\\s|$)", "ks", seleção)
+    seleção = re.sub("x(?="+classes.consoantes+")", "s", seleção)
     return seleção
