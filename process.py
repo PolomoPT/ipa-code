c4fe89e4c7690cd6b2bdf858fbbc3ff0c514970c
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-11-13 11:53:21 -0300

    trocar handling de hífens para não mexer na pronúncia

--- process.py
+++ process.py
@@ -8,10 +8,10 @@ seleção_array = seleção.split()
 
 i = 0
 while i < len(seleção_array):
-    seleção_array[i] = re.sub("-", "", seleção_array[i]) ##remoção de hífens
     pós_consoante = consoante.consoante(seleção_array[i])
     pós_vogal = vogais.vogais(pós_consoante)
     result = pós_vogal
+    result = re.sub("-", "", result) ##remoção de hífens
     print(result)
     i += 1
 
