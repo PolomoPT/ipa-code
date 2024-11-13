649499a5729a86260ff20aebd1e1ad2e882cafda
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-11-13 08:57:10 -0300

    mudar input para aceitar listas de palavras. remover hífens

--- process.py
+++ process.py
@@ -1,9 +1,18 @@
 import tonicas
 import consoante
 import vogais
+import re
 
 seleção = input()
-pós_consoante = consoante.consoante(seleção)
-pós_vogal = vogais.vogais(pós_consoante)
-result = pós_vogal
-print(result)
+seleção_array = seleção.split()
+
+i = 0
+while i < len(seleção_array):
+    seleção_array[i] = re.sub("-", "", seleção_array[i]) ##remoção de hífens
+    pós_consoante = consoante.consoante(seleção_array[i])
+    pós_vogal = vogais.vogais(pós_consoante)
+    result = pós_vogal
+    print(result)
+    i += 1
+
+
