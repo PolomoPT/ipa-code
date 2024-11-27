b938270a7ffdf76c6c7dc08ab9c23a2133aca7c3
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-11-27 11:04:36 -0300

    while True

--- process.py
+++ process.py
@@ -3,16 +3,17 @@ import consoante
 import vogais
 import re
 
-seleção = input()
-seleção_array = seleção.split()
+while True:
+    seleção = input()
+    seleção_array = seleção.split()
 
-i = 0
-while i < len(seleção_array):
-    pós_consoante = consoante.consoante(seleção_array[i])
-    pós_vogal = vogais.vogais(pós_consoante)
-    result = pós_vogal
-    result = re.sub("-", "", result) ##remoção de hífens
-    print("["+result+"]")
-    i += 1
+    i = 0
+    while i < len(seleção_array):
+        pós_consoante = consoante.consoante(seleção_array[i])
+        pós_vogal = vogais.vogais(pós_consoante)
+        result = pós_vogal
+        result = re.sub("-", "", result) ##remoção de hífens
+        print("["+result+"]")
+        i += 1
 
 
